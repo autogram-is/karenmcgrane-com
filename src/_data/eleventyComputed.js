a6c97fd8dd15d14b2e92c8127386c82af3171590
Jeff Eaton <jeff@eaton.fyi>
2023-07-25 12:28:25 -0500

    Prevent custom templates from colliding with auto-generated pages

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -2,7 +2,6 @@ const filters = require("../../utils/filters.js")
 const contentful = require("../../utils/contentful.js")
 
 module.exports = {
-
   document: async data => {
     if (data.document) return data.document
 
@@ -17,6 +16,18 @@ module.exports = {
   },
 
   permalink: data => {
+    const document = data.document ?? filters.byId(data.entries.all, data.contentId);
+
+    // This gives us a chance to halt the rendering of manually-created templates
+    // whose documents haven't been marked as 'custom'; it allows builds to proceed
+    // without permalink collisions.
+    if (document) {
+      if (data.contentId && !document.custom) {
+        console.log(`Skipping ${data.page.inputPath}; Content ID ${data.contentId} is not flagged for a custom template.`)
+        return false;
+      }
+    }
+
     if (data.permalink) return data.permalink
     if (data.document) return filters.permalink(data.document)
   },
